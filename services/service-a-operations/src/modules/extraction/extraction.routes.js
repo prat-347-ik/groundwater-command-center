e88c98c6b40bc9c3ac287ffb07a5065ef1fd3804
prat-347-ik <chavanpratik2005@gmail.com>
2026-03-10 11:51:19 +0530

    Something something...

--- services/service-a-operations/src/modules/extraction/extraction.routes.js
+++ services/service-a-operations/src/modules/extraction/extraction.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createLog, getExtractionHistory } from './extraction.controller.js';
+import { createLog, getExtractionHistory, getExtractionHistoryByRegion} from './extraction.controller.js';
 
 const router = express.Router();
 
 router.post('/', createLog);
 router.get('/:region_id', getExtractionHistory);
+router.get('/region/:regionId', getExtractionHistoryByRegion);
 
 export default router;
